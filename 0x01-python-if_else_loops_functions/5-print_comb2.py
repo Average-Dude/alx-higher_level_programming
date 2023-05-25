daf4c886de96cdbf61ce72393874ca7c1ab16308
Average-Dude <jasonannan7@gmail.com>
2023-05-25 05:13:36 -0700

    fixing fives

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(100):
-    if i == 99:
-        print(i)
-else:
-    print(f"{}".format('0' + str(i) if i < 10 else i), end=", ")
+for n in range(00, 100):
+    if n == 99:
+        print("{0:d}".format(n))
+    else:
+        print("{0:02}".format(n), end=', ')
